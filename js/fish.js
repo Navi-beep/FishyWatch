c8b3d4568a6264723e959b68ef90b203ac02f7fb
Navi-Beep <estawars30@gmail.com>
2022-12-02 16:35:16 -0600

    addedcontent

--- js/fish.js
+++ js/fish.js
@@ -31,6 +31,63 @@
 
     
         function buildfishTable(fishyData){
+            console.log('making your fish table');
+            let table = document.createElement('table');
+            table.className = 'table'
+            console.log(table)
+
+
+            let thead = document.createElement('thead');
+            let trHead = document.createElement('tr');
+            
+        
+
+            let thSpecies = document.createElement('th');
+            thSpecies.scope = 'col';
+            thSpecies.innerHTML = 'Species Name';
+
+            let thSciName = document.createElement('th');
+            thSciName.scope = 'col';
+            thSciName.innerHTML = 'Scientific Name';
+
+            let thHabitat = document.createElement('th');
+            thHabitat.scope = 'col';
+            thHabitat.innerHTML = 'Habitat';
+
+
+            let tableBody = document.createElement('tbody');
+
+            let tableRow = document.createElement('tr');
+
+            let tdSpecies = document.createElement('td');
+            tdSpecies.scope = 'row'
+            tdSpecies.innerHTML = `${fishyData[0]['Species Name']}`;
+
+            let tdSciName = document.createElement('td');
+            tdSciName.scope = 'row'
+            tdSciName.innerHTML = `${fishyData[0]['Scientific Name']}`;
+
+            let tdHabitat = document.createElement('td');
+            tdHabitat.scope = 'row'
+            tdHabitat.innerHTML = `${fishyData[0]['Habitat']}`;
+
+            tableRow.append(tdSpecies);
+            tableRow.append(tdSciName);
+            tableRow.append(tdHabitat);
+
+            tableBody.append(tableRow);
+            thead.append(trHead);
+            trHead.append(thSpecies);
+            trHead.append(thSciName);
+            trHead.append(thHabitat);
+            table.append(thead);
+            table.append(tableBody);
+
+
+            let display = document.getElementById('fishTable');
+            display.innerHTML='';
+            display.append(table);
+            console.log(display);
 
         }
 
